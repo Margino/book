97ec92992cafeb5b9b776a60165dec8548ff3dd7
Alex <marginoru@gmail.com>
2024-07-27 17:06:57 +0300

    save

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// Referenes to DOM Elements
+const prevBtn = document.querySelector('#prev-btn')
+const nextBtn = document.querySelector('#next-btn')
+const book = document.querySelector('#book')
+
+const paper1 = document.querySelector('#p1')
+const paper2 = document.querySelector('#p2')
+const paper3 = document.querySelector('#p3')
+
+// Business Logic
+let currentLocation = 0
+let numOfPage = 3
+let maxLocation = numOfPage + 1
+ 
+const goNextPage = () => {
+    
+}
+
+const goPrevP = () => {
+
+}
